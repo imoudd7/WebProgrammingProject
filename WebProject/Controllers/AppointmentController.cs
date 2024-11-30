83982c418f943580ce5a977eaa9f49748557cbf6
ahmed3bahaa <ahmedbahaamoh@gmail.com>
2024-11-30 18:51:25 +0300

    Create the Landing page minus some style

---
+++ WebProject/Controllers/AppointmentController.cs
@@ -0,0 +1,11 @@
+
+using Microsoft.AspNetCore.Mvc;
+namespace WebProject.Controllers;
+
+public class AppointmentController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ WebProject/Models/Personal.cs
@@ -0,0 +1,6 @@
+namespace WebProject.Models;
+
+public class Personal
+{
+    public int PersonalId { get; set; }
+}
