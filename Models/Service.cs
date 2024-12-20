ec5aae641d908fb453ad4bfaf77a7a41566059e5
Mahmoud Daher <moudd.daher@gmail.com>
2024-12-20 15:36:13 +0300

    Refactor and enhance Service model

--- Models/Service.cs
+++ Models/Service.cs
@@ -1,13 +1,29 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WebProject.Models;
-
-public class Service
+namespace WebProject.Models
 {
-    [Key] public int ServiceId { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public TimeSpan Duration { get; set; }
-    [Column(TypeName = "decimal(18,2)")]
-    public decimal Price { get; set; }
+    public class Service
+    {
+        [Key]  
+        public int ServiceId { get; set; }
+
+        [Required]               
+        [MaxLength(30)]          
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(500)]          
+        public string Description { get; set; } = string.Empty;
+
+        public TimeSpan Duration { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]                    
+        [DataType(DataType.Currency)]      
+        public decimal Price { get; set; }
+
+        [DefaultValue(true)]     
+        [ConcurrencyCheck]       
+        public bool IsActive { get; set; } = true; 
+    }
 }
